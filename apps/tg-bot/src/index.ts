304189e560280ed30a70717eb77607029655e6c1
Alexander <vvruspat@gmail.com>
2026-07-07 20:40:49 +0200

    Add Telegram bot config foundation

--- apps/tg-bot/src/index.ts
+++ apps/tg-bot/src/index.ts
@@ -1,1 +1,9 @@
 export const tgBotAppPackageName = "@task/tg-bot";
+
+export {
+  InvalidTelegramBotEnvironmentError,
+  loadTelegramBotConfig,
+  parseTelegramBotConfig,
+  type TelegramBotConfig,
+  type TelegramBotEnvironment,
+} from "./config.js";
